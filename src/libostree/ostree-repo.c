dea2811f8f613d8eda910c0fc3f6e1b849888793
Colin Walters <walters@verbum.org>
2016-05-11 13:32:40 -0400

    lib: Fix a leak in remote parsing
    As the docs say, `g_regex_match()` still allocates a match even if it
    returns `FALSE`.  Using `g_autoptr` is just plain better.

--- src/libostree/ostree-repo.c
+++ src/libostree/ostree-repo.c
@@ -132,7 +132,7 @@ static OstreeRemote *
 ost_remote_new_from_keyfile (GKeyFile    *keyfile,
                              const gchar *group)
 {
-  GMatchInfo *match = NULL;
+  g_autoptr(GMatchInfo) match = NULL;
   OstreeRemote *remote;
 
   static gsize regex_initialized;
@@ -158,8 +158,6 @@ ost_remote_new_from_keyfile (GKeyFile    *keyfile,
   remote->keyring = g_strdup_printf ("%s.trustedkeys.gpg", remote->name);
 
   ot_keyfile_copy_group (keyfile, remote->options, group);
-
-  g_match_info_unref (match);
 
   return remote;
 }
